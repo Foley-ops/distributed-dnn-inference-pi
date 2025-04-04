a66bc9bba2c83247ceeb4c2ca0c10d77b3f3539b
Durga Rajarajan <pragathidurga24@gmail.com>
2025-04-04 08:48:39 -0500

    added total_images initialization

--- rpc_layer_split.py
+++ rpc_layer_split.py
@@ -287,8 +287,9 @@ def run_inference(rank, world_size, model_type, batch_size, num_micro_batches, n
             logger.info("Starting inference...")
             start_time = time.time()
             
-            num_batches = 3 # hard-coded temporarily 
-
+            num_batches = 2 # hard-coded temporarily 
+            total_images = 0
+            
             with torch.no_grad():
                 for i, (images, labels) in enumerate(test_loader):
                     if i == num_batches:
